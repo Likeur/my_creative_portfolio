5835fc6675aea08c3cbdc1fd241d6171cb4bad85
Likeur <jacksimba28@gmail.com>
2023-11-24 01:47:12 +0200

    add service page

---
+++ src/app/pages/servicepage/servicepage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-servicepage',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './servicepage.component.html',
+  styles: ``
+})
+export class ServicepageComponent {
+
+}
--- src/app/pages/workpage/workpage.component.ts
+++ src/app/pages/workpage/workpage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-workpage',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavbarComponent],
   templateUrl: './workpage.component.html',
   styles: ``
 })
