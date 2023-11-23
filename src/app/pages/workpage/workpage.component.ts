0d92900fb0d65b8db7f8aaebc2d6ad9662adab2b
Likeur <jacksimba28@gmail.com>
2023-11-23 23:23:44 +0200

    add pages and components

---
+++ src/app/pages/workpage/workpage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-workpage',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './workpage.component.html',
+  styles: ``
+})
+export class WorkpageComponent {
+
+}
