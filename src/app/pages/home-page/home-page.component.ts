b84ed7782633e318691a390bcccaf23475764a6e
Likeur <jacksimba28@gmail.com>
2023-11-24 00:42:44 +0200

    starting navbar

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavbarComponent],
   templateUrl: './home-page.component.html',
   styles: ``
 })
