34ebb0d004eba9d1bcd166f97d8c257074a5365f
Likeur <jacksimba28@gmail.com>
2023-11-23 23:34:00 +0200

    add workbox component and workdetail page

---
+++ src/app/components/workbox/workbox.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-workbox',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './workbox.component.html',
+  styles: ``
+})
+export class WorkboxComponent {
+
+}
---
+++ src/app/pages/workdetail/workdetail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-workdetail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './workdetail.component.html',
+  styles: ``
+})
+export class WorkdetailComponent {
+
+}
