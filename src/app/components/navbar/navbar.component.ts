c54145c03acae79cc3f06039fb9024dd367424b5
Likeur <jacksimba28@gmail.com>
2023-11-24 01:05:13 +0200

    navbar animation work

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
-
+  navLinks = [
+    {
+      id:1,
+      title : 'Work',
+      route: '/work'
+    },
+    {
+      id:2,
+      title : 'services',
+      route: '/service'
+    },
+    {
+      id:3,
+      title : 'Contact',
+      route: '/contact'
+    }
+  ]
 }
