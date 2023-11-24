8b721e376bfcc7699b93e21bbe2dcbb578478cda
Likeur <jacksimba28@gmail.com>
2023-11-24 02:29:43 +0200

    add hero to contact section

--- src/app/pages/contactpage/contactpage.component.ts
+++ src/app/pages/contactpage/contactpage.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
-
+import { gsap } from 'gsap';
 @Component({
   selector: 'app-contactpage',
   standalone: true,
@@ -10,5 +10,20 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 })
 export default class ContactpageComponent {
+  AnimeTextHome(){
+    const tl = gsap.timeline();
 
+    tl.fromTo('.text-home',{
+      opacity: 0,
+      y: 50
+    },{
+      opacity: 1,
+      duration: 1,
+      y: 0
+    })
+  }
+
+  ngOnInit(){
+    this.AnimeTextHome();
+  }
 }
