6b43a89751de1b0214c9db0100f120153b1ec626
Likeur <jacksimba28@gmail.com>
2023-11-24 02:18:50 +0200

    add hero to work section

--- src/app/pages/workpage/workpage.component.ts
+++ src/app/pages/workpage/workpage.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
-
+import { gsap } from 'gsap';
 @Component({
   selector: 'app-workpage',
   standalone: true,
   imports: [CommonModule, NavbarComponent],
   templateUrl: './workpage.component.html',
-  styles: ``
+
 })
 export default class WorkpageComponent {
+  AnimeTextHome(){
+    const tl = gsap.timeline();
 
+    tl.fromTo('.text-home',{
+      opacity: 0,
+      y: 50
+    },{
+      opacity: 1,
+      duration: 1,
+      y: 0
+    })
+  }
+
+  ngOnInit(){
+    this.AnimeTextHome();
+  }
 }
