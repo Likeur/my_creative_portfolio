565ec42f92dbc3c6d4444bc6770b2f732ad02f5d
Likeur <jacksimba28@gmail.com>
2023-11-24 02:25:18 +0200

    add hero to service page

--- src/app/pages/servicepage/servicepage.component.ts
+++ src/app/pages/servicepage/servicepage.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
-
+import { RouterModule } from '@angular/router';
+import { gsap } from 'gsap';
 @Component({
   selector: 'app-servicepage',
   standalone: true,
-  imports: [CommonModule, NavbarComponent],
+  imports: [CommonModule, NavbarComponent, RouterModule],
   templateUrl: './servicepage.component.html',
 })
 export default class ServicepageComponent {
+  AnimeTextHome(){
+    const tl = gsap.timeline();
 
+    tl.fromTo('.text-home',{
+      opacity: 0,
+      y: 50
+    },{
+      opacity: 1,
+      duration: 1,
+      y: 0
+    })
+  }
+
+  ngOnInit(){
+    this.AnimeTextHome();
+  }
 }
